ee049bf86824db7a3d840f626085522e569964de
unknown <sandeep.misra96@gmail.com>
2020-11-19 12:26:24 +0530

    Updates

---
+++ precache-manifest.76b2af30a36c1e44e8ccdbc16883da57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7ab832a828acfb483a9ffa49541030",
+    "url": "/getGitDetails/index.html"
+  },
+  {
+    "revision": "2cef19c2e5146cf38cd8",
+    "url": "/getGitDetails/static/css/main.26cd2628.chunk.css"
+  },
+  {
+    "revision": "0f3b779a76c1857d4f81",
+    "url": "/getGitDetails/static/js/2.d2347a26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/getGitDetails/static/js/2.d2347a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cef19c2e5146cf38cd8",
+    "url": "/getGitDetails/static/js/main.32b21cc4.chunk.js"
+  },
+  {
+    "revision": "1aced26afd33c2e97a97",
+    "url": "/getGitDetails/static/js/runtime-main.d920cdc9.js"
+  },
+  {
+    "revision": "d617763fd5c08d9196374c29974e30ab",
+    "url": "/getGitDetails/static/media/octocat.d617763f.svg"
+  }
+]);
